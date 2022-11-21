cbe44306f55c423ec3496b2c97ece7bb25ab8f74
sgitwhyd <sigitwahyu.mutu@gmail.com>
2022-11-21 21:16:45 +0700

    feat: complete all design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['index.html'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: ' #212529',
+				secondary: '#DEE2E6',
+			},
+		},
 	},
 	plugins: [],
 };
